218ead8e011e4b1deac0b31e891185d08f577da9
Fernando Nicolás <fersanti2896@gmail.com>
2023-02-09 02:19:12 -0600

    Creación de proyecto desde S10 e instalación y configuración de React Route

---
+++ src/09-useContext/MainApp.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+export const MainApp = () => {
+    return (
+        <>
+            <h1>MainApp</h1>
+            <hr/>
+
+            <Outlet />
+        </>
+    )
+}
